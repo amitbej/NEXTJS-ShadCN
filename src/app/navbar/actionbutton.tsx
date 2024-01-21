9c0487c48fb4f201470c7516c25624c4da99a087
amitbej <amitbej0@gmail.com>
2024-01-22 01:32:50 +0530

    completely implemented dark mode

--- src/app/navbar/actionbutton.tsx
+++ src/app/navbar/actionbutton.tsx
@@ -11,6 +11,7 @@ import {
 
 import { LuAlignJustify } from "react-icons/lu";
 import Link from "next/link";
+import { ModeToggle } from "@/components/theme-button";
 
 const actionbutton = () => {
   return (
@@ -23,7 +24,8 @@ const actionbutton = () => {
           <SheetContent>
             <SheetHeader>
               <SheetDescription>
-                <div className="flex flex-col space-y-4 items-start w-full text-lg text-orange-400 mt-10 md:text-orange-400">
+                <ModeToggle />
+                <div className="flex flex-col items-center font-medium  space-y-4 w-full text-3xl text-orange-400 mt-10 md:text-orange-400">
                   <Link href="/"> Sign In</Link>
                   <Link href="/"> Sign Up</Link>
                   <Link href="/"> Services</Link>
@@ -36,11 +38,11 @@ const actionbutton = () => {
           </SheetContent>
         </Sheet>
       </div>
-
-      <div className="hidden md:flex md:space-x-4">
+      <div className=" items-center hidden md:flex md:space-x-4">
         <Button className="text-md" variant={"snbej"}>
           Get Started
         </Button>
+        <ModeToggle />
       </div>
     </div>
   );
